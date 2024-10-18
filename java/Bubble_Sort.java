91dc4278fd995dc147b9bb2b44a90976a967dea4
Elamathi Selvan <elamathis8060@gmail.com>
2024-10-18 18:41:10 +0530

    Bubble_Sort.java

--- java/Bubble_Sort.java
+++ java/Bubble_Sort.java
@@ -1,7 +1,19 @@
-public class Bubble_Sort {
-    public int[] sortArray(int[] nums) {
+/**
+ * Class for sorting an array using the Bubble Sort algorithm.
+ */
+public class BubbleSort {
+    
+    /**
+     * Sorts an array using the Bubble Sort algorithm.
+     *
+     * @param nums the array to be sorted
+     * @return the sorted array
+     */
+    public int[] sort(int[] nums) {
+        // Perform Bubble Sort
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = 0; j < nums.length - i - 1; j++) {
+                // Compare adjacent elements and swap if needed
                 if (nums[j] > nums[j + 1]) {
                     int temp = nums[j];
                     nums[j] = nums[j + 1];
@@ -10,5 +22,18 @@ public class Bubble_Sort {
             }
         }
         return nums;
+    }
+    
+    // Example usage
+    public static void main(String[] args) {
+        BubbleSort bubbleSort = new BubbleSort();
+        int[] nums = {5, 3, 8, 4, 2}; // Example input array
+        int[] sortedArray = bubbleSort.sort(nums);
+        
+        // Print sorted array
+        System.out.print("Sorted array: ");
+        for (int num : sortedArray) {
+            System.out.print(num + " ");
+        }
     }
 }
