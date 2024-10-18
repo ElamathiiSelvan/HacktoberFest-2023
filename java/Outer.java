a41ad30b616a3979b9e08189cf4f388d70a5541a
Elamathi Selvan <elamathis8060@gmail.com>
2024-10-18 18:47:59 +0530

    Outer.java

--- java/Outer.java
+++ java/Outer.java
@@ -1,33 +1,45 @@
-class Outer{
-	public static void main(String args[]){
-		Outer o = new Outer();
-		Outer.Inner i = o.new Inner(); //since inner class is non static, so need to call via object of outer
-		Outer.StaticInner si = new Outer.StaticInner();
-		TestInterface tf = new TestInterface(){
-			public void love(){
-				System.out.println("This to the AnonymousInner class");
-			}
-		};
-		tf.love();
-		TestInterface tf2 = ()->{  //only applicable for functional interface
-				System.out.println("This to the AnonymousInner class using lamba function");
-		};
-		tf2.love();
-	}
-	Outer(){
-		System.out.println("This to the outer class");
-	}
-	class Inner{
-		Inner(){
-			System.out.println("This to the Inner class");
-		}
-	}
-	static class StaticInner{
-		StaticInner(){
-			System.out.println("This to the StaticInner class");
-		}
-	}
+class Outer {
+    public static void main(String[] args) {
+        Outer o = new Outer(); // Create an instance of Outer
+        Outer.Inner inner = o.new Inner(); // Create an instance of Inner class
+        Outer.StaticInner staticInner = new Outer.StaticInner(); // Create an instance of StaticInner class
+
+        // Create an instance of an anonymous inner class
+        TestInterface anonymousInner = new TestInterface() {
+            public void love() {
+                System.out.println("This is the Anonymous Inner class");
+            }
+        };
+        anonymousInner.love();
+
+        // Create an instance of an anonymous inner class using a lambda expression
+        TestInterface lambdaInner = () -> {
+            System.out.println("This is the Anonymous Inner class using a lambda function");
+        };
+        lambdaInner.love();
+    }
+
+    // Constructor for Outer class
+    Outer() {
+        System.out.println("This is the Outer class");
+    }
+
+    // Non-static inner class
+    class Inner {
+        Inner() {
+            System.out.println("This is the Inner class");
+        }
+    }
+
+    // Static inner class
+    static class StaticInner {
+        StaticInner() {
+            System.out.println("This is the Static Inner class");
+        }
+    }
 }
-interface TestInterface{
-	void love();
+
+// Functional interface for demonstration
+interface TestInterface {
+    void love(); // Method to be implemented
 }
